1e942151f6ca146cd1422167179e6c6f174dc198
Marcell Forgó <forgoo.marcell0@gmail.com>
2025-12-02 22:26:49 +0100

    api link updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const app=express()
 //app.use(cors({ origin: "http://localhost:5173" }))
-//app.use(cors({ origin:"https://fm06-recipe-backend.vercel.app"}))
+app.use(cors({ origin:"https://fm06-recipe-backend.vercel.app/api"}))
 app.use(cors())
 
 app.use(express.json({limit:"5mb"}))
